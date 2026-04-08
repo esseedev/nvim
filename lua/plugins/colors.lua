8ba4265c17fffa9132ea2cf3d04c3b8417908faa
esseedev <krzysztoflyda@outlook.com>
2026-04-08 08:51:55 +0200

    change bg to black

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -5,6 +5,28 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      local function set_black_background()
+        local groups = {
+          'Normal',
+          'NormalNC',
+          'NormalFloat',
+          'SignColumn',
+          'EndOfBuffer',
+          'TelescopeNormal',
+          'TelescopeBorder',
+          'TelescopePromptNormal',
+          'TelescopePromptBorder',
+          'TelescopeResultsNormal',
+          'TelescopeResultsBorder',
+          'TelescopePreviewNormal',
+          'TelescopePreviewBorder',
+        }
+
+        for _, group in ipairs(groups) do
+          vim.api.nvim_set_hl(0, group, { bg = '#000000' })
+        end
+      end
+
       require('rose-pine').setup {
         styles = {
           italic = false,
@@ -12,6 +34,11 @@ return {
       }
 
       vim.cmd.colorscheme 'rose-pine'
+
+      set_black_background()
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        callback = set_black_background,
+      })
     end,
   },
 }
